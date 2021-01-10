cfca2e99c3a2ee39896cdaaea33a083a3b614c2c
Yann Klein <yann.klein@me.com>
2021-01-10 13:33:07 +0900

    boilerplate trhreejs with webpack 5

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './styles/index.scss';
+
+import initThree from './three/three-init';
+
+console.log(
+  `%c Yann's ThreeJS boilerplate`,
+  `background: linear-gradient( -70deg, rgba(9,9,121,0.6) 11.2%, rgba(144,6,161,0.6) 53.7%, rgba(0,212,255,0.6) 100.2% );`
+);
+
+const threeEl = document.querySelector('.three-js');
+if (threeEl) {
+  initThree(threeEl);
+}
