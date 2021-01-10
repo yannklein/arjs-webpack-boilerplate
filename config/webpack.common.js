adf5cadaa56bd73f35a6799f4f5494ec54ac9b20
Yann Klein <yann.klein@me.com>
2021-01-10 14:19:55 +0900

    path correction in webpack config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -12,7 +12,7 @@ module.exports = {
   output: {
     path: paths.build,
     filename: '[name].bundle.js',
-    publicPath: '/',
+    publicPath: '',
   },
 
   // Customize the webpack build process
