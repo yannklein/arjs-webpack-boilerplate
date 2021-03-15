d470e615c640db09e899829c83f7eac8ad315ceb
Yann Klein <yann.klein@me.com>
2021-03-15 14:36:34 +0900

    change ThreeJS to ARJS in title here and there

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import './styles/index.scss';
 import initThree from './three/three-init';
 
 console.log(
-  `%c Yann's ThreeJS boilerplate`,
+  `%c Yann's ARJS boilerplate`,
   `background: linear-gradient( -70deg, rgba(9,9,121,0.6) 11.2%, rgba(144,6,161,0.6) 53.7%, rgba(0,212,255,0.6) 100.2% );`
 );
 
